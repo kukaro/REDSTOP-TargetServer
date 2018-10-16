a1845adbca1bc5bfd12937ac8677aff79b79e19f
kukaro <justkukaro@naver.com>
2018-10-16 16:40:28 +0900

    call

--- express/app.js
+++ express/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var io = require('socket.io').listen(3100);
+var io = require('socket.io');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
